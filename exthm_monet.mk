1728538804ac37a9711a0af619982495c26facbb
WesternLi <61825420+WesternLi@users.noreply.github.com>
2021-06-25 01:03:52 +0800

    Update exthm_monet.mk

--- exthm_monet.mk
+++ exthm_monet.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/monet/device.mk)
 
 # Inherit some common RevengeOS stuff.
-$(call inherit-product, vendor/exthm/config/common.mk)
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := exthm_monet
